85a896ad5f5315e3ca98c6951fa27d40468bc77e
imtiana <imjihyung@gmail.com>
2016-08-27 23:00:17 -0700

    text sending only works upto 160 chars -_- -_- -_- -_-

--- app/src/main/java/com/autochime/autochimeapplication/MainActivity.java
+++ app/src/main/java/com/autochime/autochimeapplication/MainActivity.java
@@ -3,7 +3,6 @@ package com.autochime.autochimeapplication;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 
 import com.autochime.autochimeapplication.fragments.AddContactFragment;
 
@@ -15,12 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // just testing
-        SmsManager smsManager1 = SmsManager.getDefault();
-        smsManager1.sendTextMessage("5554", null, "ugh", null, null);
-        smsManager1.sendTextMessage("5556", null, "ugh2", null, null); // send to both just in case the emulators get mixed up during demos
-
         SMSManager smsManager = new SMSManager();
-        smsManager.send();
+        smsManager.sendHardcode();
 
         // Show contact selector
 //        FragmentTransaction ft = getFragmentManager().beginTransaction();
--- app/src/main/java/com/autochime/autochimeapplication/SMSManager.java
+++ app/src/main/java/com/autochime/autochimeapplication/SMSManager.java
@@ -7,25 +7,29 @@ import android.telephony.SmsManager;
  * Created by tianaim on 8/27/16.
  */
 public class SMSManager {
+    SmsManager smsManager = null;
 
-    public final String dummyPhoneNo = "5554"; // first emulator
-    public final String dummyPhoneNo2 = "5556"; // second emulator
-    public final String dummyMessageBody = "Your friend Kelly may be in danger -  listen to this audio recording from her phone. http://recording.autochime.com/r/c3a564b6 If you think she is in danger, click here to call 911. If you don't, ignore this message.";
+    private String dummyPhoneNo = null;
+    private String dummyPhoneNo2 = null;
+    private String dummyMessageBody = null;
 
     public SMSManager() {
+        smsManager = SmsManager.getDefault();
+
+        dummyPhoneNo = "5554"; // first emulator
+        dummyPhoneNo2 = "5556"; // second emulator
+        dummyMessageBody = "Your friend Kelly may be in danger -  listen to this recording from her phone. https://rec.autochime.com/c3a564b6. If you think she is in danger....";
     }
 
-    // default method with hard coded messages
-    public void send() {
-//        if (Build.VERSION.SDK_INT < 23) {
+    public void sendHardcode() {
+        if (Build.VERSION.SDK_INT < 23) {
             try {
-                SmsManager smsManager = SmsManager.getDefault();
                 smsManager.sendTextMessage(dummyPhoneNo, null, dummyMessageBody, null, null);
                 smsManager.sendTextMessage(dummyPhoneNo2, null, dummyMessageBody, null, null); // send to both just in case the emulators get mixed up during demos
             } catch (Exception e) {
                 e.printStackTrace();
             }
-//       }
+       }
     }
 
     public void send(String userName, String phoneNo, String messageBody) {
