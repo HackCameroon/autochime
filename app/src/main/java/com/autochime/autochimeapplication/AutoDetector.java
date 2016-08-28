a823defbd4f845ffd4f98dfce699dc0d0569c776
Wilbur Yu <yuwilbur@gmail.com>
2016-08-28 00:57:37 -0700

    Added AutoDetector to StateMachine

---
+++ app/src/main/java/com/autochime/autochimeapplication/AutoDetector.java
@@ -0,0 +1,140 @@
+package com.autochime.autochimeapplication;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+interface AutoDetectListener {
+    void onAutoDetect();
+}
+
+/**
+ * Created by kl on 8/27/16.
+ * <p/>
+ * Samples
+ * 1. to initiate listener call mViolentDetector = new ViolentDetector(this);
+ * <p/>
+ * 2. to stop listner call mViolentDetector = null;
+ * <p/>
+ * 3. to stop Fake ring tone, call mViolentDetector.mute();
+ */
+public class AutoDetector {
+    SensorEventListener mSL;
+    Ringtone mFakeRingtone;
+    Context mContext;
+    private boolean mPlaying = false;
+
+    public AutoDetector(Context c) {
+        mContext = c;
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+        mFakeRingtone = RingtoneManager.getRingtone(c, notification);
+        regVioListner();
+    }
+
+    private List<AutoDetectListener> mListeners = new ArrayList<AutoDetectListener>();
+    public void addListener(AutoDetectListener listener) {
+        mListeners.add(listener);
+    }
+    private void OnDetect() {
+        for (AutoDetectListener listener : mListeners) {
+            listener.onAutoDetect();
+        }
+    }
+
+    /**
+     * register violent listener
+     */
+    private void regVioListner() {
+        Log.v("ViolentDetection", "regVioListner");
+        mSL = new SensorEventListener() {
+            private float mAccel = 0; // acceleration apart from gravity
+            private float mAccelCurrent = SensorManager.GRAVITY_EARTH; // current
+            // acceleration
+            // including
+            // gravity
+            private float mAccelLast = SensorManager.GRAVITY_EARTH; // last
+            // acceleration
+            // including
+            // gravity
+
+            @Override
+            public void onSensorChanged(SensorEvent se) {
+                final long shs = 10;
+
+                float x = se.values[0];
+                float y = se.values[1];
+                float z = se.values[2];
+                // M.l("x=" + x + " y=" + y + " z=" + z);
+                mAccelLast = mAccelCurrent;
+                mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y));
+                // mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z
+                // * z));
+                float delta = mAccelCurrent - mAccelLast;
+                mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+                // if (mAccel * shs > 5 * 20) {
+                if (!mPlaying && mAccel * shs > 10 * 20) {
+                    mPlaying = true;
+//					M.l("x=" + x + " y=" + y + " z=" + z);
+//					M.l("mAccel=" + mAccel + " mAccelLast=" + mAccelLast
+//							+ " mAccelCurrent=" + mAccelCurrent);
+                    mFakeRingtone.play();
+
+//					final SoundMeter sm = new SoundMeter();
+//					sm.start();
+//					final Handler handler = new Handler();
+//					handler.postDelayed(new Runnable() {
+//						@Override
+//						public void run() {
+//							if (sm.getAmplitude()> certainValue)
+//								mFakeRingtone.play();
+//							sm.stop();
+//						}
+//					}, 100);
+
+
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+
+            }
+        };
+        SensorManager sm = (SensorManager)
+                mContext.getSystemService(Context.SENSOR_SERVICE);
+        sm.registerListener(mSL, sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    private void unregVioListner() {
+        if (mSL != null) {
+            // M.l("ss0.");
+            SensorManager sm = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+            sm.unregisterListener(mSL);
+            mSL = null;
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        unregVioListner();
+    }
+
+    public void mute() {
+        mFakeRingtone.stop();
+        mPlaying = false;
+    }
+}
--- app/src/main/java/com/autochime/autochimeapplication/StateMachine.java
+++ app/src/main/java/com/autochime/autochimeapplication/StateMachine.java
@@ -1,5 +1,7 @@
 package com.autochime.autochimeapplication;
 
+import android.content.Context;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,17 +20,23 @@ interface TransitionListener {
 /**
  * Created by Wilbur on 08/27/16.
  */
-public class StateMachine {
+public class StateMachine implements AutoDetectListener {
     private State mState = State.Default;
 
     private AudioRecorder mAudioRecorder = null;
+    private AutoDetector mAutoDetector = null;
+    private Alarm mAlarm = null;
 
-    public StateMachine() {
+    public StateMachine(Context context) {
         mAudioRecorder = new AudioRecorder();
+        mAutoDetector = new AutoDetector(context);
+        mAlarm = new Alarm(context);
     }
 
+    @Override public void onAutoDetect() { CheckState(); }
+
     private List<TransitionListener> mListeners = new ArrayList<TransitionListener>();
-    public void addTransitionListener(TransitionListener listener) {
+    public void addListener(TransitionListener listener) {
         mListeners.add(listener);
     }
     private void OnTransition() {
@@ -42,17 +50,21 @@ public class StateMachine {
         switch (mState) {
             case Default:
                 mAudioRecorder.StopRecord();
+                mAlarm.SetState(false);
                 break;
             case AutoAlarm:
                 mAudioRecorder.StartRecord();
+                mAlarm.SetState(true);
                 break;
             case ManualAlarm:
                 mAudioRecorder.StartRecord();
+                mAlarm.SetState(false);
                 break;
             case Notify:
                 Transition(State.PostNotify);
                 break;
             case PostNotify:
+                mAlarm.SetState(false);
                 break;
             default:
                 break;
