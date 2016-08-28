50ba2b34800e662c2c99bac48457db0154d21a29
imtiana <imjihyung@gmail.com>
2016-08-28 01:34:53 -0700

    remove version check from smsmanger. add dummy webpage

--- app/src/main/java/com/autochime/autochimeapplication/SMSManager.java
+++ app/src/main/java/com/autochime/autochimeapplication/SMSManager.java
@@ -14,25 +14,29 @@ public class SMSManager {
     private String dummyPhoneNo = null;
     private String dummyPhoneNo2 = null;
     private String dummyMessageBody = null;
+    private String dummyDownloadBody = null;
+
 
     public SMSManager() {
         smsManager = SmsManager.getDefault();
 
         dummyPhoneNo = "5554"; // first emulator
         dummyPhoneNo2 = "5556"; // second emulator
-        dummyMessageBody = "Your friend Kelly may be in danger -  listen to this recording from her phone. https://rec.autochime.com/c3a564b6. If you think she is in danger, click here to send her help. If you think she is fine, ignore this message.";
+        dummyMessageBody = "Your friend Kelly may be in danger -  listen to this recording from her phone. https://rec.autochime.com/c3a564b6. Location: <27.1960, 78.0212>. <If you think she is in danger, click here to send her help. If you think she is fine, ignore this message.";
+        dummyDownloadBody = "<Download audio>";
     }
 
     public void sendHardcode() {
-        if (Build.VERSION.SDK_INT < 23) {
-            try {
-                ArrayList<String> parts = smsManager.divideMessage(dummyMessageBody);
-                smsManager.sendMultipartTextMessage(dummyPhoneNo, null, parts, null, null);
-                smsManager.sendMultipartTextMessage(dummyPhoneNo2, null, parts, null, null);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-       }
+        try {
+            ArrayList<String> parts = smsManager.divideMessage(dummyMessageBody);
+            smsManager.sendMultipartTextMessage(dummyPhoneNo, null, parts, null, null);
+            smsManager.sendMultipartTextMessage(dummyPhoneNo2, null, parts, null, null);
+            smsManager.sendTextMessage(dummyPhoneNo, null, dummyDownloadBody, null, null);
+            smsManager.sendTextMessage(dummyPhoneNo2, null, dummyDownloadBody, null, null);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void send(String userName, String phoneNo, String messageBody) {
